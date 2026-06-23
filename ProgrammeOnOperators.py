f9d9bfd8861ade1bcf5e3999704ef9d5ea11bc72
theerthaprem13-dotcom <theerthaprem13@gmail.com>
2026-06-23 12:55:20 +0400

    Programme on operators

---
+++ ProgrammeOnOperators.py
@@ -0,0 +1,54 @@
+#Operator: it is a symbol that performs an operation on values
+#Operand: it is the values on which the operators work
+
+a=10
+b=5
+
+result=a+b
+print("The value of result is:",result)
+
+#Arithmetic operators(+,-,*,/,%,**(power eg.2**3),//(floor division, rounded of value))
+
+a1=10
+b1=3
+print(a1+b1)
+print(a1-b1)
+print(a1*b1)
+print(a1/b1)
+print(a1**b1)
+print(a1%b1)
+print(a1//b1)
+
+#Comparison operators: compares two values and return either true or false (==(equal to),!=(Not equal to),>,<,>=,<=)
+
+a3=10
+b3=5
+print(a3==b3)
+print(a3!=b3)
+print(a3>b3)
+print(a3<b3)
+print(a3>=b3)
+print(a3<=b3)
+
+#Assignement operators:it is used to assign values to variables
+#1 +=  -------> x+=5 //x=x+5
+#2 -= -----> x-=5 // x=x-5
+#3 *= -----> x*=5 //x=x*5
+#4 /= -----> x/=5 // x=x/5
+
+score=15
+score+=10
+print(score)
+
+score1=20
+score1-=10
+print(score1)
+
+
+
+
+
+
+
+
+
