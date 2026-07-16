16dcbf282db1e7bebdf67b0f8736badffc180a74
theerthaprem13-dotcom <theerthaprem13@gmail.com>
2026-07-16 14:00:44 +0530

    math

---
+++ Assignement1.py
@@ -0,0 +1,8 @@
+import math
+print("The floor and ceiling value of 23.56 are:"+str(math.ceil(23.56))+","+str(math.floor(23.56)))
+x=10
+y=-52
+print("The value of x after copying the sign from y is:"+str(math.copysign(x,y)))
+
+print("Absolute value of -96 and 56 are:"+str(math.fabs(-96))+","+str(math.fabs(56)))
+print("The GCD of 24 and 56:"+str(math.gcd(24,56)))
